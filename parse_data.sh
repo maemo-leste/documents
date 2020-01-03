c04e1d585eb350175d20f23656b875b02c0d405c
parazyd <parazyd@dyne.org>
2020-01-03 16:52:05 +0100

    Update.

--- parse_data.sh
+++ parse_data.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-jq -r '.[] | .name + "," + .url + "," + .license.spdx_id' < data.json > bom.csv
+
+echo 'Name,URL,License' > bom.csv
+jq -r '.[] | .name + "," + .html_url + "," + .license.spdx_id' < data.json >> bom.csv
